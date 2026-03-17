7ca5fe29a27e72aa7496618837e5611476d40850
dionisiocerdena8-cloud <dionisiocerdena8@gmail.com>
2026-03-17 21:45:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -329,4 +329,6 @@ if __name__ == '__main__':
             db.session.commit()
             print("Nagawa na ang default admin: admin@oathofcare.com / admin123")
 
-    app.run(debug=True, port=5000)
+    # BAGO: Binuksan natin ang pinto (0.0.0.0) para makita ni Render ang server mo!
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
